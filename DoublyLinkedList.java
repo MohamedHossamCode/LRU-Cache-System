318c38421fce135cec63d0ca08e859ae60bc21c4
yelwageh-droid <yelwageh@gmail.com>
2026-05-05 19:32:08 +0300

    Implement insertFront method in DoublyLinkedList
    Added insertFront method to insert a node at the front of the list.

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -7,4 +7,25 @@ public class DoublyLinkedList {
         this.tail = null;
     }
 
+    public void insertFront(Node newNode){
+        // في حالة عدم وجود اي عنصر
+        if (this.head==null){
+            // خليت النود الجديدة هي الهيد و التيل مع بعض
+            head = tail = newNode;
+        }
+        //في حالة وجود عناصر
+        else{
+            // هخلي الاتنين يشاوروا علي بعض
+            newNode.next=head;
+            head.prev=newNode;
+            // خليت النود الجديدة هي الهيد
+            head=newNode;
+            // خليت الهيد قبلها null عشان دي مش ليست دائرية circle
+            head.prev=null;
+        }
+    }
+
+
+
+    
 }
