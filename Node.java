c458b2b7a173b407219ea672361158580a5c5fd6
yelwageh-droid <yelwageh@gmail.com>
2026-05-06 12:14:05 +0300

    Implement DoublyLinkedList initialization and insertFront method

---
+++ Node.java
@@ -0,0 +1,52 @@
+public class Node {
+    private int key;
+    private int value;
+    private Node prev;
+    private Node next;
+
+    public Node() {
+        this.prev = null;
+        this.next = null;
+    }
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+
+}
