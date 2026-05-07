ac5da059dd0154edf5a211bb4be495251f4d5398
Mohamed Hossam <mohamedhossam742007@gmail.com>
2026-05-07 23:15:40 +0300

    add put method with collision handling using chainning to use chainning : i added variable in Node class nextInHash

--- HashTable.java
+++ HashTable.java
@@ -11,4 +11,18 @@ public class HashTable {
         return Math.abs(key) % capacity; 
     }
     
+    public void put(int key, int value) {
+        int index = hash(key);
+        // collision handling using chaining
+        Node head = table[index];
+        if (head == null) table[index] = new Node(key,value);
+        Node current = head;
+        while (current != null) {
+            if (current.getKey() == key) {
+                current.setValue(value); // update existing key
+                return;
+            }
+            current = current.nextInHash;
+        }
+    }
 }
